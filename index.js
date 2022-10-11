ebcd68f262cf05eba6078310df846c43b94d9d89
Damia <adebitandamola02@gmail.com>
2022-10-11 01:13:23 +0100

    code refactor

--- index.js
+++ index.js
@@ -1,136 +1,81 @@
-import data from "./data.json" assert {type:'json'};
+let data = []
+const filterButton = document.querySelectorAll(".btn")
+const buttons = [...filterButton]
 
-const sectionContainer = document.querySelector(".tracker");
-
-
-
-const dailyBtn = document.querySelector(".daily");
-const weeklyBtn = document.querySelector(".weekly");
-const monthlyBtn = document.querySelector(".monthly");
-const filterBtn = document.querySelectorAll(".btn");
-
-window.addEventListener("DOMContentLoaded", () => {
-   displayActivity(data)
-})
-
-filterBtn.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        filterBtn.forEach((buttons) => {
-            if (buttons !== btn) {
-                buttons.classList.remove("active");
-            }
+const detectclickedButton = (array) => {
+    array.forEach(button => {
+        button.addEventListener("click", () => {
+            activateButtonClicked(button)
+            const clickedOption = button.dataset.id;
+            displayActivityData(clickedOption)
         })
-        btn.classList.toggle("active");
     })
-})
+}
 
-filterBtn.forEach((button) => {
-    button.addEventListener("click", (e) => {
-        const category = e.currentTarget.dataset.id;
-        console.log(category);
-        const activityCategory = data.filter((activityItem) => {
-            if (activityCategory === category) {
-                
-            }
-        })
+//fetch data
 
-        console.log(activityCategory);
-    })
-})
+const loadFetchedData = async () => {
+  const response = await fetch('./data.json');
+  const fetchedData = await response.json();
+  data = fetchedData
+  buttons[1].click()
+}
 
-function displayActivity(activityItems) {
-    let displayItems = activityItems.map((items) => {
-        return `<div class="event-tracker ${items.title}-tracker">
-        <div class="box">
-            <img src="./images/icon-${items.imgName}.svg" class="icon" alt="">
+const activateButtonClicked = (button) => {
+    buttons.forEach(btn => btn.classList.remove("active"));
+    button.classList.add('active')
+}
+
+const cleanActivities = () => {
+  const activities = document.querySelectorAll('.event-tracker')
+  activities.forEach(item => item.remove())
+}
+
+const displayActivityData = (clickedOption) => {
+  cleanActivities()
+  const activityTracker = document.querySelector('.tracker')
+
+  const measureTimeframeData = (time) => {
+    if (time === "daily") {
+      return "Yesterday"
+    } else if(time === "weekly") {
+      return "Last Week"
+    } else {
+      return "Last Month"
+    }
+  }
+
+  
+  data.forEach((activity) => {
+    const title = activity.title;
+    const activityClassName = title.toLowerCase().replace(" ", "-");
+    const timeframes = activity.timeframes[clickedOption];
+    const prevTimeframes  =  measureTimeframeData(clickedOption);
+    const activityContainer = document.createElement("div");
+    activityContainer.classList.add("event-tracker", activityClassName);
+    const sectionContent = `<div class="box">
+            <img src="./images/icon-${activityClassName}.svg" class="icon" alt="">
         </div>
         <div class="details">
             <div class="activity">
-                <p>${items.title}</p>
+                <p>${title}</p>
                 <img src="./images/icon-ellipsis.svg" class="ellipsis" alt="">
             </div>
             <div class="activity-time">
-                <h1 class="hour">${items.timeframes.weekly.current}hours</h1>
+                <h1 class="hour">${timeframes.current}hrs</h1>
                 <div class="date">
-                    <span class="time">Last Week</span> - <span class="hour2">${items.timeframes.weekly.previous}hrs</span>
+                    <span class="time">${prevTimeframes}</span> - <span class="time2">${timeframes.previous}hrs</span>
                 </div>
             </div>
-        </div>
-    </div>`
-    })
-    displayItems = displayItems.join("");
-    sectionContainer.innerHTML = displayItems;
-}
+        </div>`
+        
+    activityContainer.innerHTML = sectionContent
+    activityTracker.append(activityContainer)
+  })
+} 
 
-// dailyBtn.addEventListener("click", () => {
-//     let displayItems = data.map((items) => {
-//         return `<div class="event-tracker ${items.title}-tracker">
-//         <div class="box">
-//             <img src="./images/icon-${items.title}.svg" class="icon" alt="">
-//         </div>
-//         <div class="details">
-//             <div class="activity">
-//                 <p>${items.title}</p>
-//                 <img src="./images/icon-ellipsis.svg" class="ellipsis" alt="">
-//             </div>
-//             <div class="activity-time">
-//                 <h1 class="hour">${items.timeframes.daily.current}hours</h1>
-//                 <div class="date">
-//                     <span class="time">Yesterday</span> - <span class="hour2">${items.timeframes.daily.previous}hrs</span>
-//                 </div>
-//             </div>
-//         </div>
-//     </div>`
-//     })
-//     displayItems = displayItems.join("");
-//     sectionContainer.innerHTML = displayItems;
-// })
 
-// weeklyBtn.addEventListener("click", () => {
-//     let displayItems = data.map((items) => {
-//         return `<div class="event-tracker ${items.title}-tracker">
-//         <div class="box">
-//             <img src="./images/icon-${items.title}.svg" class="icon" alt="">
-//         </div>
-//         <div class="details">
-//             <div class="activity">
-//                 <p>${items.title}</p>
-//                 <img src="./images/icon-ellipsis.svg" class="ellipsis" alt="">
-//             </div>
-//             <div class="activity-time">
-//                 <h1 class="hour">${items.timeframes.weekly.current}hours</h1>
-//                 <div class="date">
-//                     <span class="time">Last week</span> - <span class="hour2">${items.timeframes.weekly.previous}hrs</span>
-//                 </div>
-//             </div>
-//         </div>
-//     </div>`
-//     })
-//     displayItems = displayItems.join("");
-//     sectionContainer.innerHTML = displayItems;
-// })
+detectclickedButton(buttons)
+loadFetchedData()
 
-// monthlyBtn.addEventListener("click", () => {
-//     let displayItems = data.map((items) => {
-//         return `<div class="event-tracker ${items.title}-tracker">
-//         <div class="box">
-//             <img src="./images/icon-${items.title}.svg" class="icon" alt="">
-//         </div>
-//         <div class="details">
-//             <div class="activity">
-//                 <p>${items.title}</p>
-//                 <img src="./images/icon-ellipsis.svg" class="ellipsis"  alt="">
-//             </div>
-//             <div class="activity-time">
-//                 <h1 class="hour">${items.timeframes.monthly.current}hours</h1>
-//                 <div class="date">
-//                     <span class="time">Last Month</span> - <span class="hour2">${items.timeframes.monthly.previous}hrs</span>
-//                 </div>
-//             </div>
-//         </div>
-//     </div>`
-//     })
-//     displayItems = displayItems.join("");
-//     sectionContainer.innerHTML = displayItems;
-// })
 
